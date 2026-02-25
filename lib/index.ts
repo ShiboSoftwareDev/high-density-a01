1c0000590bb8d6bf9d96c587db6d2d7975725c68
seveibar <seveibar@gmail.com>
2026-02-24 18:13:15 -0800

    add rust/wasm implementation

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,5 @@
 export { HighDensitySolverA01 } from "./HighDensitySolverA01/HighDensitySolverA01"
+export {
+  HighDensitySolverA01WasmEngine,
+  initHighDensitySolverWasm,
+} from "./HighDensitySolverA01WasmEngine/HighDensitySolverA01WasmEngine"
